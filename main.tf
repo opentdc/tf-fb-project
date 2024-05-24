25fa92c4b5948489879834d517f92ff5e77d03da
Bruno Kaiser <bruno@bkaiser.ch>
2024-05-24 16:08:57 +0200

    readding provider

--- main.tf
+++ main.tf
@@ -15,7 +15,26 @@
 
 terraform {
   required_version = "~> 1.8"
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.0"
+    }
+  }
 }
+
+# Configure the provider not to use the specified project for quota check.
+# This provider should only be used during project creation and initializing services.
+provider "google-beta" {
+  alias                 = "no_user_project_override"
+  user_project_override = false
+}
+
+# Configure the provider that uses the new project's quota.
+provider "google-beta" {
+  user_project_override = true
+}
+
 
 # Create a new Google Cloud project.
 resource "google_project" "default" {
